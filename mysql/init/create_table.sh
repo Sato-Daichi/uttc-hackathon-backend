ab5afba2ee933241db5678c94b28b790f0080cb6
Daichi Sato <satodai370054@gmail.com>
2023-06-02 23:42:29 +0900

    不要なDBを削除し、テーブル作成ファイルを更新

--- mysql/init/create_table.sh
+++ mysql/init/create_table.sh
@@ -1,42 +1,67 @@
 #!/bin/sh
 
 CMD_MYSQL="mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}"
-$CMD_MYSQL -e "create table app_user
-(
-    user_id   char(26)    not null
-        primary key,
-    user_name varchar(20) not null,
-    password  varchar(50) not null
+
+$CMD_MYSQL -e "CREATE TABLE channels (
+  id varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  description text NOT NULL,
+  create_user_id varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  workspace_id varchar(255) NOT NULL
 );"
 
-$CMD_MYSQL -e "create table channel
-(
-    channel_id   char(26)    not null
-        primary key,
-    channel_name varchar(30) not null
+$CMD_MYSQL -e "CREATE TABLE messages (
+  id varchar(255) NOT NULL,
+  text text NOT NULL,
+  channel_id varchar(255) NOT NULL,
+  user_id varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );"
 
-$CMD_MYSQL -e "create table channel_member
-(
-    id         char(26)    not null
-        primary key,
-    channel_id varchar(20) not null,
-    user_id    char(26)    not null
+$CMD_MYSQL -e "CREATE TABLE replies (
+  id varchar(255) NOT NULL,
+  text text NOT NULL,
+  channel_id varchar(255) NOT NULL,
+  user_id varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  parent_id varchar(255) NOT NULL
 );"
 
-$CMD_MYSQL -e "create table message
-(
-    message_id      char(26)     not null
-        primary key,
-    user_id         char(26)     not null,
-    message_content varchar(150) not null
+$CMD_MYSQL -e "CREATE TABLE users_channels (
+  id varchar(255) NOT NULL,
+  channel_id varchar(255) NOT NULL,
+  user_id varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );"
 
-$CMD_MYSQL -e "create table reply
-(
-    reply_id      char(26)     not null
-        primary key,
-    user_id       char(26)     not null,
-    reply_content varchar(150) not null,
-    message_id    char(26)     not null
+$CMD_MYSQL -e "CREATE TABLE users_workspaces (
+  id varchar(255) NOT NULL,
+  workspace_id varchar(255) NOT NULL,
+  user_id varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );"
+
+$CMD_MYSQL -e "CREATE TABLE users (
+  id varchar(26) NOT NULL,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);"
+
+$CMD_MYSQL -e "CREATE TABLE workspaces (
+  id varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);"
+
+$CMD_MYSQL -e "INSERT INTO users (id, username, password, email) VALUES ('00000000000000000000000001', 'sato', 'sato', 'sato@gmail.com');"
+$CMD_MYSQL -e "INSERT INTO users (id, username, password, email) VALUES ('00000000000000000000000002', 'suzuki', 'suzuki', 'suzuki@gmail.com');"
